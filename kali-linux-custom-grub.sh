520fc8143119efdab9e4d3bb7ccbd1c46c654e17
thetqweb <84143034+thetqweb@users.noreply.github.com>
2021-05-13 20:13:20 +0300

    v1.0.0

---
+++ kali-linux-custom-grub.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+mkdir /home/grub/
+mkdir /home/grub/backup/
+scp /etc/default/grub /home/grub/backup/
+
+rm  /etc/default/grub
+
+echo GRUB_DEFAULT=0 >> /etc/default/grub
+echo GRUB_TIMEOUT=21 >> /etc/default/grub
+echo GRUB_DISTRIBUTOR=`lsb_release -i -s 2> /dev/null || echo Debian`  >> /etc/default/grub
+echo GRUB_CMDLINE_LINUX_DEFAULT=\"quiet/etc/default/grub\" >> /home/grub/grub
+echo GRUB_CMDLINE_LINUX=""  >> /etc/default/grub
+
+update-grub
+
